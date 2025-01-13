0825783d36813c620aa831c768473e8365a6af68
Rem0o <remi.mercier.software@gmail.com>
2025-01-13 08:59:55 -0500

    Update format-json.ps1

--- format-json.ps1
+++ format-json.ps1
@@ -160,11 +160,10 @@ foreach ($baseFile in $baseFiles) {
                     if ($formattedTranslationJson[$i] -ne $translationJson[$i]) {
                         $hex1 = [System.Convert]::ToString([System.Convert]::ToInt32($formattedTranslationJson[$i]), 16)
                         $hex2 = [System.Convert]::ToString([System.Convert]::ToInt32($translationJson[$i]), 16)
-                        Write-Output "Difference at position {$i}: (0x$hex1) vs (0x$hex2)"
+                        $problem += "Difference at position {$i}: (0x$hex1) vs (0x$hex2)" + $newline
                         break;
                     }
                 }
-
 
                 if ($Fix) {
                     Write-Output "Formatting [$translationFile]"
@@ -179,14 +178,12 @@ foreach ($baseFile in $baseFiles) {
         $exitCode = -1;
         $problem += "Formatting for [$baseFile] is wrong" + $newline
 
-        
-
         $length = [math]::Min($formattedBaseJson.Length, $baseJson.Length)
         for ($i = 0; $i -lt $length; $i++) {
             if ($formattedBaseJson[$i] -ne $baseJson[$i]) {
                 $hex1 = [System.Convert]::ToString([System.Convert]::ToInt32($formattedBaseJson[$i]), 16)
                 $hex2 = [System.Convert]::ToString([System.Convert]::ToInt32($baseJson[$i]), 16)
-                Write-Output "Difference at position {$i}: (0x$hex1) vs (0x$hex2)"
+                $problem += "Difference at position {$i}: (0x$hex1) vs (0x$hex2)" + $newline
                 break;
             }
         }
